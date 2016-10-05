b9b0b354cf15d50e8fcf7f2b5182a88ae30006e4
Magda <mmosoiu@pentalog.fr>
2016-10-05 11:01:05 +0300

    Add New Method

--- Fundamentals.Tests/Class1.cs
+++ Fundamentals.Tests/Class1.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace Fundamentals.Tests
 {
     class Class1
     {
+        public void test()
+        {
+            Console.WriteLine("Evrica");
+        }
     }
 }
